fdb7b2db28ab133fe49a846c0a22a6a8d74a3bb6
Gal Zalan <Gal Zalan@HUNTER>
2019-12-12 09:18:11 +0200

    Add project files.

---
+++ Problem1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    class Program
+    {
+        public enum GenderType
+        {
+            Female = 1,
+            Male = 2
+        }
+        static void Main(string[] args)
+        {
+            DateTime CurrentDate = DateTime.Now;
+            int age;
+            int? gender = null;
+
+            Console.Write("Please provide the year when yoo were born:");
+            string input = Console.ReadLine();
+            int BirthYear = int.Parse(input);
+
+            Console.Write("Please provide the month when yoo were born:");
+            input = Console.ReadLine();
+            int BirthMonth = int.Parse(input);
+
+            Console.Write("Please provide the day when yoo were born:");
+            input = Console.ReadLine();
+            int BirthDay = int.Parse(input);
+
+            Console.Write("Please provide Your gender. M for male or F for female:");
+            input = Console.ReadLine();
+
+
+            if (input.Equals("f") || input.Equals("F"))
+            {
+                gender = (int)GenderType.Female;
+            }
+            else if (input.Equals("m") || input.Equals("M"))
+            {
+                gender = (int)GenderType.Male;
+            }
+
+            DateTime BirthDate = new DateTime(BirthYear, BirthMonth, BirthDay);
+            TimeSpan diff = CurrentDate.Subtract(BirthDate);
+            age = diff.Days;
+            age = age / 365;
+
+            if (gender != null)
+            {
+                if (gender == 1)
+                {
+                    if (age > 63)
+                    {
+                        Console.WriteLine("You were born on "+ BirthDate.ToString("dd/MMM/yyyy") + " and you are "+age+" years old retired female.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You were born on " + BirthDate.ToString("dd/MMM/yyyy") + " and you are " + age + " years old female who will retire at the age 63");
+                    }
+                }
+                else
+                {
+                    if (age > 65)
+                    {
+                        Console.WriteLine("You were born on " + BirthDate.ToString("dd/MMM/yyyy") + " and you are " + age + " years old retired male.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You were born on " + BirthDate.ToString("dd/MMM/yyyy") + " and you are " + age + " years old male who will retire at the age 65");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Problem2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String FirstName;
+            String LastName;
+            String FullName = "";
+            string input;
+
+            Console.Write("Please enter your first name:");
+            input = Console.ReadLine();
+            FirstName = input;
+
+            Console.Write("Please enter your Last name:");
+            input = Console.ReadLine();
+            LastName = input;
+
+            FullName = string.Concat(FirstName.ToUpper()," ", LastName.ToUpper());
+            Console.WriteLine("Your full name is: "+ FullName);
+
+            Console.Write("Please enter a text:");
+            input = Console.ReadLine();
+            String text1 = input;
+
+            Console.Write("Please enter a string you want to replace:");
+            input = Console.ReadLine();
+            string OldString = input;
+
+            Console.Write("Please enter a string that will replace '" + OldString + "' :");
+            input = Console.ReadLine();
+            string NewString = input;
+
+            text1 = text1.Replace(OldString, NewString);
+            Console.WriteLine("The new text is: " + text1);
+
+            Console.Write("Please enter a text:");
+            input = Console.ReadLine();
+            string text2 = input;
+
+            Console.Write("Please enter a string you want to remove:");
+            input = Console.ReadLine();
+            OldString = input;
+
+            int indexOfOldString = text2.IndexOf(OldString);
+            String Newtext2 = text2.Remove(indexOfOldString, OldString.Length); 
+            Console.WriteLine("The new text is: " + Newtext2);
+
+
+            Console.Write("Please enter a string or just press enter: ");
+            input = Console.ReadLine();
+            string text3 = input;
+
+            if (string.IsNullOrEmpty(text3))
+            {
+                Console.WriteLine("String null sau gol.");
+            }
+            else
+            {
+                Console.WriteLine("Stringul are continut: " + text3);
+            }
+
+        }
+    }
+}
